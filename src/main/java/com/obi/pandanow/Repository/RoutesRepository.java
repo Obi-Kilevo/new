fa5cf0088de082d234ee898f4471d061a3b991df
Obi-Kilevo <ombenkilevo157@gmail.com>
2026-01-28 15:40:20 +0800

    Update application code

---
+++ src/main/java/com/obi/pandanow/Repository/RoutesRepository.java
@@ -0,0 +1,9 @@
+package com.obi.pandanow.Repository;
+
+import com.obi.pandanow.Entity.RoutesEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoutesRepository extends JpaRepository<RoutesEntity, Long> {
+}
