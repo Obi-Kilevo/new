c6f9a80dfd06e3a0b04aa6187e4d011972dc9a11
Obi-Kilevo <ombenkilevo157@gmail.com>
2026-01-28 21:14:16 +0800

    Update VideoControllers

--- src/main/java/com/obi/pandanow/PandaNowApplication.java
+++ src/main/java/com/obi/pandanow/PandaNowApplication.java
@@ -2,8 +2,12 @@ package com.obi.pandanow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.obi.pandanow.Repository")  // ADD THIS LINE
+@EntityScan("com.obi.pandanow.Entity")  // ADD THIS LINE
 public class PandaNowApplication {
 
     public static void main(String[] args) {
@@ -11,3 +15,18 @@ public class PandaNowApplication {
     }
 
 }
+
+
+//package com.obi.pandanow;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class PandaNowApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(PandaNowApplication.class, args);
+//    }
+//
+//}
